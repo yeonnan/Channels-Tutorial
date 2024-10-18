1c0c54d2188ebe09cecb47a66d1a0580c504c467
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-10-18 16:23:46 +0900

    django channels

--- pjtChannels/settings.py
+++ pjtChannels/settings.py
@@ -88,9 +88,12 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
     }
 }
 
